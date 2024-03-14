30ac00df0da1a9880e0de3bc2096ec8994124b45
simyt <e0036029@u.nus.edu>
2024-03-14 10:44:00 +0800

    first commit

---
+++ src/main/java/sg/nus/iss/d16walkthrough/D16walkthroughApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.d16walkthrough;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D16walkthroughApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D16walkthroughApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/iss/d16walkthrough/D16walkthroughApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.d16walkthrough;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D16walkthroughApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
